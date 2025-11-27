ca3a036ffab05c8aa490f424346473210b68c4cc
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2025-11-27 10:39:42 +0000

    Add PWA Support

---
+++ service-workers.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "uni-novel-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/android-chrome-192x192.png", // Ícones
+  "/android-chrome-512x512.png"
+];
+
+// Instalando o Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativando o Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptando requisições
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
